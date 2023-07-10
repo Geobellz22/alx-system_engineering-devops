e36206ae0c02e67255c669084586b684d54de816
Geobelz22 <st377126@gmail.com>
2023-07-10 07:30:35 -0700

    Task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,8 +2,8 @@
 # create a custom HTTP header response with puppet
 
 exec { 'command':
-  command => 'apt-get -y update
-  apt-get -y install nginx
+  command => 'apt-get -y update;
+  apt-get -y install nginx;
   sudo sed -i "/listen 80 default_server;/ a add_header x-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   service nginx restart',
   provider => shell,
