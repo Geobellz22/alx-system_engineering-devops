9ee27ca4ad12837be4b100ca57bb2657098cc738
Geobellz22 <st377126@gmail.com>
2023-08-09 21:33:34 +0000

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #using strace, to find out why apache is returning 500 error
 #
 exec {'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => 'usr/local/bin/:/bin/'
 } 
