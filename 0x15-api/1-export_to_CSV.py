0f9edfe16c9bccf714fc76ea50e20a963ca25538
Geobellz22 <st377126@gmail.com>
2023-08-11 22:55:16 +0000

    Task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
 
     user = requests.get(url + "users/{}".format(user_id)).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open('{}.csv'.format(user.get("Id")), 'w', newline='') as csvfile:
         data_writer = csv.writer(readfile, quoting=csv.QUOTE_ALL)
