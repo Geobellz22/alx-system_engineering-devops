4a015b89dbbd389032da98f35485a5d54d3bd9f5
Geobellz22 <st377126@gmail.com>
2023-08-17 21:03:47 +0000

    Task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# Change the OS connfiguration so that is possible
+# To login with the holberton
+
+exec { 'hard':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'soft':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
